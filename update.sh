bfc8b1d9ab2e7a420cc7ba3a7e95c1729d24127b
Thomas DuVally <tduvally@brown.edu>
2020-04-15 11:38:12 -0400

    Make tests, renames, new code

--- update.sh
+++ update.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 # Shell script to run update
+KEY="Standard_QA"
+YUMOPTS=""
+YUMBIN=/usr/bin/yum
+YUMARGS="-y -e 0"
 
 # Subscribe to Satellite updates (PROD)
+sat_reg() {
+# RHN Register
+  rpm --import http://unixrepo.services.brown.edu/217521F6.txt
+  rpm -Uvh https://psatcit.services.brown.edu/pub/katello-ca-consumer-latest.noarch.rpm
+  subscription-manager clean
+  subscription-manager register --org BrownCIS --activationkey "$KEY"
+  yum -y -d 0 -e 0 install katello-agent
+}
 
 # Run updates
+yum_update() {
+  $YUMBIN $YUMARGS $YUMOPTS update
+}
+
   # Failure 
     # exit with message and status
 
