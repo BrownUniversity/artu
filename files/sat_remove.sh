fd97c18da4e2f2225d2e2fdf3e31e87678790fe9
Thomas DuVally <tduvally@brown.edu>
2020-04-29 14:58:23 -0400

    I'm forgetting bash, fixed var assign

--- files/sat_remove.sh
+++ files/sat_remove.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 # Command to remove from Satellite using password
 
-$1=HOSTNAME
-$2=USER
-$3=PASSWORD
+HOSTNAME=$1
+USER=$2
+PASSWORD=$3
 
 /usr/bin/curl -X DELETE -s -k -u $USER:'$PASSWORD' https://psatcit.services.brown.edu/api/v2/hosts/$HOSTNAME.services.brown.edu
