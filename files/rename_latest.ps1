97913276b9e7a81089b264f0d5a76c70edb731be
Thomas DuVally <tduvally@brown.edu>
2020-04-29 11:09:37 -0400

    confirm to false

--- files/rename_latest.ps1
+++ files/rename_latest.ps1
@@ -2,4 +2,4 @@ param ($vs_hostname, $vs_name, $vs_password, $inventory_hostname, $newname)
 
 Connect-VIServer -Server $vs_hostname -User $vs_name -Password $vs_password | Out-Null
 
-Set-VM -VM $inventory_hostname -ToTemplate -Name $newname
+Set-VM -VM $inventory_hostname -ToTemplate -Name $newname -Confirm False
