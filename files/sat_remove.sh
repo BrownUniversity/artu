de3bf175952eacf453dd808b0771d0bb10717a00
Thomas DuVally <tduvally@brown.edu>
2020-04-29 15:00:59 -0400

    quotes are dumb

--- files/sat_remove.sh
+++ files/sat_remove.sh
@@ -5,4 +5,4 @@ HOSTNAME=$1
 USER=$2
 PASSWORD=$3
 
-/usr/bin/curl -X DELETE -s -k -u $USER:'$PASSWORD' https://psatcit.services.brown.edu/api/v2/hosts/$HOSTNAME.services.brown.edu
+/usr/bin/curl -X DELETE -s -k -u $USER:$PASSWORD https://psatcit.services.brown.edu/api/v2/hosts/$HOSTNAME.services.brown.edu
